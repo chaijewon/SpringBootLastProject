76400642753999d0e560db7a88e7a9fb30f30798
chaijewon <vcandjava@nate.com>
2026-01-05 14:50:36 +0900

    1111

--- src/main/java/com/sist/web/controller/BusanController.java
+++ src/main/java/com/sist/web/controller/BusanController.java
@@ -1,29 +1,27 @@
 package com.sist.web.controller;
-import java.util.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.sist.web.service.*;
-import com.sist.web.vo.*;
+import com.sist.web.service.BusanService;
+import com.sist.web.vo.BusanVO;
+import com.sist.web.vo.SeoulVO;
 
 import lombok.RequiredArgsConstructor;
-/*
- *   MVC : 오라클 / 컨트롤러 / JSP 
- *         --------------- Vue / React 
- *         
- *         => SQL / 사용자가 어떤 데이터를 보낼지
- *         
- */
+
 @Controller
 @RequiredArgsConstructor
-public class SeoulController {
-  private final SeoulService sService;
+public class BusanController {
+  private final BusanService bService;
 
-  @GetMapping("/seoul/list")
-  public String seoul_location(
+  @GetMapping("/busan/list")
+  public String busan_location(
     @RequestParam(name="page",required = false) String page,
     @RequestParam("cno") int cno, 
     Model model
@@ -36,13 +34,13 @@ public class SeoulController {
 	  Map map=new HashMap();
 	  map.put("start", (curpage-1)*12);
 	  map.put("contenttype",cno);
-	  List<SeoulVO> list=sService.seoulListData(map);
-	  for(SeoulVO vo:list)
+	  List<BusanVO> list=bService.busanListData(map);
+	  for(BusanVO vo:list)
 	  {
 		  String[] addrs=vo.getAddress().split(" ");
 		  vo.setAddress(addrs[0]+" "+addrs[1]);
 	  }
-	  int totalpage=sService.seoulTotalPage(cno);
+	  int totalpage=bService.busanTotalPage(cno);
 	  final int BLOCK=10;
 	  int startPage=((curpage-1)/BLOCK*BLOCK)+1;
 	  int endPage=((curpage-1)/BLOCK*BLOCK)+BLOCK;
@@ -52,12 +50,12 @@ public class SeoulController {
 	  
 	  
 	  String name="";
-	  if(cno==12) name="서울 관광지";
-	  else if(cno==14) name="서울 문화시설";
-	  else if(cno==15) name="서울 축제 & 공연";
-	  else if(cno==32) name="서울 숙박";
-	  else if(cno==38) name="서울 쇼핑";
-	  else if(cno==39) name="서울 음식";
+	  if(cno==12) name="부산 관광지";
+	  else if(cno==14) name="부산 문화시설";
+	  else if(cno==15) name="부산 축제 & 공연";
+	  else if(cno==32) name="부산 숙박";
+	  else if(cno==38) name="부산 쇼핑";
+	  else if(cno==39) name="부산 음식";
 	  
 	  model.addAttribute("name", name);
 	  model.addAttribute("list", list);
@@ -67,16 +65,8 @@ public class SeoulController {
 	  model.addAttribute("endPage", endPage);
 	  model.addAttribute("cno", cno);
 	  
-	  model.addAttribute("main_jsp", "../seoul/list.jsp");
+	  model.addAttribute("main_jsp", "../busan/list.jsp");
 	  return "main/main";
   }
   
 }
-
-
-
-
-
-
-
-
